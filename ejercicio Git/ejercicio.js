554b40f887c4fd183658d665acc0c97cdec2c702
Erika-Andrade <erikaandradesuz@gmail.com>
2024-08-07 11:52:59 -0500

    el boton cambia la imagen

--- ejercicio Git/ejercicio.js
+++ ejercicio Git/ejercicio.js
@@ -1,3 +1,6 @@
 saludar=function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo=document.getElementById("imgSaludo");
+    cmpImagenSaludo.src="./imagenes/idk.gif";
     console.log("Ingresa a saludar");
 }
