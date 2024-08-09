47c8abc10042e52a9d3cc19b0a6bbd57ace15ca2
Erika-Andrade <erikaandradesuz@gmail.com>
2024-08-08 22:52:00 -0500

    funcion recuperarTexto ok

---
+++ utilitarios/saludo.js
@@ -0,0 +1,12 @@
+saludar=function(){
+    //recuperar el valor de la caja de texto txtNombre<>
+    let nombre=recuperarTexto("txtNombre");
+    //recuperar el valor de la caja de texto txtApellido<>
+    let apellido=recuperarTexto("txtApellido");
+}
+recuperarTexto=function (idComponente) {
+    let componente, valorIngresado;
+    componente=document.getElementById(idComponente);
+    valorIngresado=componente.value;
+    return valorIngresado;    
+}
