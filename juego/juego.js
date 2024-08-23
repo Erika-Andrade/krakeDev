7dec635ef781570b2ade87aac8052153cfc3f939
Erika-Andrade <erikaandradesuz@gmail.com>
2024-08-23 11:11:22 -0500

    estilos css y boton nueva partida

--- juego/juego.js
+++ juego/juego.js
@@ -1,3 +1,5 @@
+let puntosUsuario=0;
+let puntosComputador=0;
 jugar=function(seleccionado){
     let elemento,ruta,ganador;
     elemento=generarElemento();
@@ -8,7 +10,23 @@ jugar=function(seleccionado){
         mostrarTexto("lblGanador","EMPATE");
     }else if(ganador==1){
         mostrarTexto("lblGanador","PERDISTE LA PARTIDA!!");
+        puntosComputador+=1;
+        mostrarTexto("lblPuntosComp","Computadora: "+puntosComputador);
     }else if(ganador==2){
         mostrarTexto("lblGanador","GANASTE LA PARTIDA!!");
+        puntosUsuario+=1;
+        mostrarTexto("lblPuntosUsuario","Usuario: "+puntosUsuario);
     }
+    if(puntosUsuario==5){
+        mostrarTexto("lblWinPuntos","HAS GANADO EL JUEGO");
+    }else if(puntosComputador==5){
+        mostrarTexto("lblWinPuntos","EL COMPUTADOR TE HA VENCIDO");
+    }
+}
+limpiar=function(){
+    mostrarTexto("lblPuntosComp","Computadora: 0");
+    mostrarTexto("lblPuntosUsuario","Usuario: 0");
+    mostrarImagen("imgSelecCompu","");
+    mostrarTexto("lblGanador","");
+    mostrarTexto("lblWinPuntos","");
 }
