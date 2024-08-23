dce8b7fa5a0dc4a04131ce2400b96ef6c5cd1c14
Erika-Andrade <erikaandradesuz@gmail.com>
2024-08-23 16:32:35 -0500

    funcion invertirCadena y recorrerCadena

---
+++ bucles/ejercicioCadenas.js
@@ -0,0 +1,33 @@
+ejecutarPrueba1=function(){
+    let mensaje;
+    mensaje=recuperarTexto("txtCadena");
+    recorrerCadena(mensaje);
+}
+
+
+
+recorrerCadena=function(cadena){//<>
+    let caracter;
+    for(let posicion=0;posicion<cadena.length;posicion++){
+        caracter=cadena.charAt(posicion)
+        console.log("Caracter "+caracter+" posicion "+posicion);
+    }
+
+    for(let posicion=0;posicion<=cadena.length-1;posicion++){
+        caracter=cadena.charAt(posicion)
+        console.log("Caracter "+caracter+" posicion "+posicion);
+    }
+}
+ejecutarPrueba2=function(){
+    let mensaje;
+    mensaje=recuperarTexto("txtCadena");
+    invertirCadena(mensaje);
+}
+invertirCadena=function(cadena){
+    let resultado="";
+    for(let posicion=cadena.length;posicion>=0,posicion--;){
+        resultado+=cadena.charAt(posicion);
+    }
+    mostrarTexto("lblInvertido",resultado);
+
+}
