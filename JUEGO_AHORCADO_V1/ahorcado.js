fe328ae686b63bcd1d86632e505594e8c8c037ab
Erika-Andrade <erikaandradesuz@gmail.com>
2024-08-23 23:59:55 -0500

    Paso 1 funcion guardarPalabra verificado

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,9 +1,26 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+let palabraSecreta;
 esMayuscula=function(caracter){
     let ascCaracter=caracter.charCodeAt(0);
     if(ascCaracter>=65 && ascCaracter<=90){
         return true;
     }else{
         return false;
+    }
+}//<>
+guardarPalabra=function(){
+    let mensaje,caracter,contMayus=0;
+    mensaje=recuperarTexto("txtSecreta");
+    for(let i=0;i<5;i++){
+        caracter=mensaje.charAt(i);
+        if(esMayuscula(caracter)){
+            contMayus++;
+        }
+    }
+    if((mensaje.length)==5 & contMayus==5){
+        palabraSecreta=mensaje;
+        console.log(mensaje);
+    }else{
+        alert("Debe ingresar una palabra de 5 letras mayusculas.");
     }
 }
